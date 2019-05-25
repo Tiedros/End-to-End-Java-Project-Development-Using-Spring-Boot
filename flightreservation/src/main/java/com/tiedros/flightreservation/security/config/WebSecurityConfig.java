a1f03103716ce5dd4f54201ea74884101502deee
Tiedros Abrha <tiedrosabrha@gmail.com>
2019-05-24 22:30:08 -0500

    code fix for E2E test issues

--- flightreservation/src/main/java/com/tiedros/flightreservation/security/config/WebSecurityConfig.java
+++ flightreservation/src/main/java/com/tiedros/flightreservation/security/config/WebSecurityConfig.java
@@ -2,6 +2,7 @@ package com.tiedros.flightreservation.security.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -19,9 +20,14 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
-				.antMatchers("/showReg", "/", "/index.html", "/registerUser", "/login", "/showLogin", "/login/*")
+				.antMatchers("/showReg", "/", "/index.html", "/registerUser", "/login", "/showLogin", "/login/*","/reservations/*")
 				.permitAll().antMatchers("/admin/showAddFlight").hasAnyAuthority("ADMIN").anyRequest().authenticated()
 				.and().csrf().disable();
+	}
+	
+	@Bean//(name = "authenticationManager")
+	public AuthenticationManager authenticationManagerBean() throws Exception {
+		return super.authenticationManagerBean();
 	}
 
 }
